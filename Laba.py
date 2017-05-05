e92eec086ce31dd02f07c84f1266f42e6025da9f
Viktor Boiarchuk <victorboiarchuk@gmail.com>
2017-05-05 11:08:09 +0300

    Modified Laba

--- Laba.py
+++ Laba.py
@@ -3,17 +3,15 @@ def laba_description():
     group = input("Enter your group: ")
     name = input("Enter your name:  ")
     all_laba_data = [len(group), len(name), len(laba_name)]
-    for i in all_laba_data:
-        if i == max(all_laba_data):
-            table_length = i
-            return table_length, laba_name, group, name
+    table_length = max(all_laba_data)
+    return table_length, laba_name, group, name
 
 
 def laba_print():
     table_length, laba_name, group, name = laba_description()
     print("{0}{1}{0}".format('*', '*'*table_length))
-    for dumpy_i in [laba_name, group, name]:
-        print("{0}{1}{0}".format('*', dumpy_i), (table_length-len(dumpy_i))*'*', sep='')
+    for elem in [laba_name, group, name]:
+        print("{0}{1}{0}{2}".format('*', elem, (table_length - len(elem)) * '*'))
     print("{0}{1}{0}".format('*', '*'*table_length))
 
 laba_print()
